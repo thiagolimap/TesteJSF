281d03d30fc2672d4e2b35505382ced266e3e875
Admin <Admin@WS1>
2013-10-29 17:49:13 -0300

    creado Util

---
+++ TesteJSF/src/main/java/com/cosa/util/HibernateUtil.java
@@ -0,0 +1,29 @@
+package com.cosa.util;
+
+import com.sun.org.apache.xalan.internal.xsltc.runtime.BasisLibrary;
+import javax.validation.Configuration;
+import org.hibernate.SessionFactory;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ *
+ * @author Admin
+ */
+public class HibernateUtil {
+    
+   //private static final SessionFactory sessionfactory;
+   
+ //  public static final String HIBERNATE_SESSION = "hibernate_session";
+   
+  // static {
+    //   try{
+      //     System.out.println("Abriendo una sessión");
+        //   Configuration configuration = new Configuration().configure();
+          // ServiceRegistryBuilder serviceRegistryBuilder = new ServiceRegistryBuilder();
+         //  sessionfactory = configuration.buildSessionFactory()
+  //     }catch(Exception e){
+           
+    //   }
+ //  }
+
+}

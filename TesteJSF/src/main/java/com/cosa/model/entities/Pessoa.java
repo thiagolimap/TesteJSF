4db8602d162fdada5630a5fb5f35042f0822b566
Thiago <Thiago@Thiago-PC>
2013-10-31 22:28:55 -0300

    Adicionado entidades SEXO e Pessoa

--- TesteJSF/src/main/java/com/cosa/model/entities/Pessoa.java
+++ TesteJSF/src/main/java/com/cosa/model/entities/Pessoa.java
@@ -12,8 +12,10 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
+import org.hibernate.annotations.ForeignKey;
 
 /**
  *
@@ -51,6 +53,11 @@ public class Pessoa implements Serializable{
     @Temporal(javax.persistence.TemporalType.DATE)
     private Date dataCadrastro;    
 
+    @ManyToOne(optional = false)
+    @ForeignKey (name = "PessoaSexo")
+    private Pessoa pessoa;
+    
+    
     public Pessoa() {
     }
 
---
+++ TesteJSF/src/main/java/com/cosa/model/entities/Sexo.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.cosa.model.entities;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import org.hibernate.annotations.ForeignKey;
+
+/**
+ *
+ * @author Thiago
+ */
+@Entity
+@Table (name="sexo")
+public class Sexo implements Serializable{
+    
+     private static final long serialVersionUID = 1L;
+     
+     
+     @Id
+     @GeneratedValue
+     @Column(name = "IdSexo", nullable = false)
+     private Integer idSexo;
+     
+     @Column(name = "Descrição", unique = true, nullable = false, length= 9)
+     private String descricao;
+
+     
+     @OneToMany(mappedBy = "sexo",fetch= FetchType.LAZY)
+     @ForeignKey (name = "PessoaSexo")
+     private List<Pessoa> pessoas;
+
+    public List<Pessoa> getPessoas() {
+        return pessoas;
+    }
+
+    public void setPessoas(List<Pessoa> pessoas) {
+        this.pessoas = pessoas;
+    }
+     
+     
+    public Sexo() {
+    }
+
+    public Integer getIdSexo() {
+        return idSexo;
+    }
+
+    public void setIdSexo(Integer idSexo) {
+        this.idSexo = idSexo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 73 * hash + (this.idSexo != null ? this.idSexo.hashCode() : 0);
+        hash = 73 * hash + (this.descricao != null ? this.descricao.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Sexo other = (Sexo) obj;
+        if (this.idSexo != other.idSexo && (this.idSexo == null || !this.idSexo.equals(other.idSexo))) {
+            return false;
+        }
+        if ((this.descricao == null) ? (other.descricao != null) : !this.descricao.equals(other.descricao)) {
+            return false;
+        }
+        return true;
+    }
+
+   
+    
+    
+     
+     
+     
+    
+}
